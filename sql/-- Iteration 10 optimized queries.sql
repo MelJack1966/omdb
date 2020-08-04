653d7186a784b0d7dfd1a1f72914cd4de2bf0ab1
MelJack1966 <46384386+MelJack1966@users.noreply.github.com>
2020-08-03 19:28:24 -0500

    Update -- Iteration 10 optimized queries.sql

--- sql/-- Iteration 10 optimized queries.sql
+++ sql/-- Iteration 10 optimized queries.sql
@@ -4,4 +4,10 @@
 SELECT * FROM people WHERE last_name LIKE "hitchcock%"
 
 --optimized version
-EXPLAIN SELECT stage_name FROM people WHERE last_name = "Hitchcock"
+ EXPLAIN SELECT stage_name FROM people WHERE last_name = "Hitchcock"
+ 
+ --Mel
+ SELECT * FROM movies WHERE english_name LIKE 'm%'
+ 
+ --optimized version
+ EXPLAIN SELECT native_name FROM movies WHERE english_name LIKE 'm%'
