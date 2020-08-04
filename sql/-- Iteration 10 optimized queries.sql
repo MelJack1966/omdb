ab3f3a187fc155ee27c929441a2011ae5b2ab694
limaalphamikepapa <65194915+limaalphamikepapa@users.noreply.github.com>
2020-08-03 19:18:46 -0500

    Create -- Iteration 10 optimized queries.sql

---
+++ sql/-- Iteration 10 optimized queries.sql
@@ -0,0 +1,7 @@
+-- Iteration 10 optimized queries
+
+-- Jed
+-- SELECT * FROM people WHERE last_name LIKE "hitchcock%"
+
+--optimized version
+-- EXPLAIN SELECT stage_name FROM people WHERE last_name = "Hitchcock"
